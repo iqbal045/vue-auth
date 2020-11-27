f001bf3c6676e58516bda987446094b0e49d8a12
iqbal045 <iqbal.ultimate@gmail.com>
2020-11-27 14:07:52 +0600

    - install start vue template

---
+++ src/config/AppRoutes.vue
@@ -0,0 +1,10 @@
+<script>
+import Home from '../pages/Home.vue'
+
+const routes = [
+  { path: '*', redirect: '/home' },
+  { path: '/home', component: Home }
+]
+
+export default routes;
+</script>
---
+++ src/config/AppSidebarMenu.vue
@@ -0,0 +1,13 @@
+<script>
+    const appSidebarMenu = [{
+        text: 'Navigation',
+        is_header: true
+    },
+    {
+        url: '/home',
+        icon: 'fa fa-home',
+        text: 'Home'
+    }];
+
+    export default appSidebarMenu;
+</script>
